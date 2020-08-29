71f5d413eaabfe7058bf0ed2b12477a5c4720817
MT0609 <thang123lk@gmail.com>
2020-08-29 20:38:07 +0700

    Updates

---
+++ precache-manifest.003a8219adea4d8478045a47a5c9d1d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3383cb87c026681ceb6f72a71a36fb",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "f3a89a3e9c4fcf9563b5",
+    "url": "/todo-app/static/css/main.e45407f4.chunk.css"
+  },
+  {
+    "revision": "373d27b03a245dc80f16",
+    "url": "/todo-app/static/js/2.ee47c432.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app/static/js/2.ee47c432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3a89a3e9c4fcf9563b5",
+    "url": "/todo-app/static/js/main.94fc182f.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  },
+  {
+    "revision": "dc2fd78b5fb691cf5e4e6551f03001e4",
+    "url": "/todo-app/static/media/complete.dc2fd78b.png"
+  },
+  {
+    "revision": "4a10b842c26b03817df83a424d9b8c2c",
+    "url": "/todo-app/static/media/doing.4a10b842.png"
+  },
+  {
+    "revision": "99fe34de5b11928ff6bf53a8b98ee084",
+    "url": "/todo-app/static/media/task.99fe34de.png"
+  }
+]);
